888d491c8cb0c8a20953593fad8bcd9ce8df974e
ZZy <2122424930@qq.com>
2020-08-26 16:09:47 +0800

    账号

--- src/main/java/com/aaa/controller/AccountController.java
+++ src/main/java/com/aaa/controller/AccountController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import sun.management.counter.AbstractCounter;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
 @Controller
@@ -23,5 +25,29 @@ public class AccountController {
     public Account  Login(@RequestBody Account account){
         Account accounts=accountDao.selectOne(account);
         return accounts;
+    }
+
+    @RequestMapping("account_listAll")
+    @ResponseBody
+    public List<Map<String,Object>>  listAll(){
+        return accountDao.listAll();
+    }
+
+    @RequestMapping("account_update")
+    @ResponseBody
+    public int  update(Account account){
+        return accountDao.update(account);
+    }
+
+    @RequestMapping("account_add")
+    @ResponseBody
+    public int  add(Account account){
+        return accountDao.add(account);
+    }
+
+    @RequestMapping("account_updatePwd")
+    @ResponseBody
+    public int  updatePwd(Account account){
+        return accountDao.updatePwd(account);
     }
 }
--- src/main/java/com/aaa/dao/AccountDao.java
+++ src/main/java/com/aaa/dao/AccountDao.java
@@ -1,11 +1,25 @@
 package com.aaa.dao;
 
 import com.aaa.entity.Account;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface AccountDao extends tk.mybatis.mapper.common.Mapper<Account> {
+    @Select("select a.acc_id,a.acc_name,a.acc_pwd,a.acc_state,a.emp_id,e.emp_id,e.emp_name from account a left join employee e on a.emp_id=e.emp_id")
+    public List<Map<String,Object>> listAll();
 
+    @Insert("insert into account(acc_name,acc_pwd,acc_state,emp_id) values(#{acc_name},#{acc_pwd},#{acc_state},#{emp_id})")
+    Integer add(Account account);
+
+    @Update("update account set acc_state=#{acc_state} where acc_id=#{acc_id}")
+    Integer update(Account account);
+
+    @Update("update account set acc_pwd='123456'  where acc_id=#{acc_id}")
+    Integer updatePwd(Account account);
 }
