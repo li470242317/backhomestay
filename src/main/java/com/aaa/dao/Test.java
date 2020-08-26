78fb633e9ce5c09cef7dcf9fe8b272911383276f
yc-4237 <1248127422@qq.com>
2020-08-26 09:35:18 +0800

    account,functions,power实体类

--- src/main/java/com/aaa/dao/Test.java
+++
@@ -1,8 +0,0 @@
-package com.aaa.dao;
-
-import com.aaa.entity.Room_type;
-import tk.mybatis.mapper.common.Mapper;
-@org.apache.ibatis.annotations.Mapper
-public interface Test extends Mapper<Room_type> {
-
-}
