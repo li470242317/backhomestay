d3c749b68f5a5370932af6c8fdd15488941469b9
lmy0804 <2193003875@qq.com>
2020-08-28 09:22:28 +0800

    评价管理

---
+++ src/main/java/com/aaa/controller/Order_appraiseController.java
@@ -0,0 +1,29 @@
+package com.aaa.controller;
+
+import com.aaa.dao.Order_appraiseDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin
+@Controller
+@RequestMapping("Order_appraise")
+public class Order_appraiseController {
+    @Resource
+    Order_appraiseDao order_appraiseDao;
+    @RequestMapping("Order_appraise_query")
+    @ResponseBody
+    public List<Map<String,Object>> query(){
+        return order_appraiseDao.query();
+    }
+    @RequestMapping("Order_appraise_delete")
+    @ResponseBody
+    public int delete(int oa_id){
+        return order_appraiseDao.deleteByPrimaryKey(oa_id);
+    }
+}
---
+++ src/main/java/com/aaa/dao/Order_appraiseDao.java
@@ -0,0 +1,26 @@
+package com.aaa.dao;
+
+import com.aaa.entity.Order_appraise;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Select;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@org.apache.ibatis.annotations.Mapper
+public interface Order_appraiseDao extends Mapper<Order_appraise> {
+    @Select("select o.*,od.od_name,od.od_card,h.hou_name,\n" +
+            "            h.hou_address,h.hou_row,h.hou_details,c.cli_name,oa.oa_id,oa.cli_id,oa.or_cover,oa.oa_date,oa.or_details\n" +
+            "            from order_appraise oa \n" +
+            "\t\t\t\tleft join orders o on oa.or_id=o.or_id\n" +
+            "            left join house h on o.hou_id=h.hou_id \n" +
+            "            left join client c on h.cli_id=c.cli_id\n" +
+            "            left join order_details od\n" +
+            "            on od.or_id=o.or_id\n" +
+            "\n" +
+            "\n")
+    List<Map<String,Object>> query();
+
+
+}
